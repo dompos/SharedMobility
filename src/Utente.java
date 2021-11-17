88b67e77a46c69f959e2ce9daec8c8ebbc640c60
SekBo <defilippo.giuseppe96@gmail.com>
2021-11-17 12:44:57 +0100

    Versione Aggiornata Utente

--- src/Utente.java
+++ src/Utente.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.Objects;
+import java.util.*;
 
 public class Utente {
     /**
@@ -13,12 +12,12 @@ public class Utente {
     private String codiceFiscale;
     private double creditoResiduo;
     private boolean casco;
-    private ArrayList<Patente> patentiUtente;
+    private Patente patentiUtente;
 
     /**
      * Costruttore Classe Utente
      */
-    public Utente(String nome, String cognome, String codiceFiscale, double creditoResiduo, boolean casco, ArrayList<Patente> patenti) {
+    public Utente(String nome, String cognome, String codiceFiscale, double creditoResiduo, boolean casco, Patente patenti) {
         this.nome = nome;
         this.cognome = cognome;
         this.codiceFiscale = codiceFiscale;
@@ -78,13 +77,177 @@ public class Utente {
         this.casco = casco;
     }
 
-    public ArrayList<Patente> getPatentiUtente() {
+    public Patente getPatentiUtente() {
         return patentiUtente;
     }
 
-    public void setPatentiUtente(ArrayList<Patente> patente) {
+    public void setPatentiUtente(Patente patente) {
         this.patentiUtente = patente;
     }
+
+    /**
+     * Creo ID Utente
+     */
+
+    public int idGen() {
+
+        int id = this.nome.hashCode() + this.cognome.hashCode();
+        int length = String.valueOf(id).length();
+        int Max_Length = 5;
+        if(String.valueOf(id).length()>Max_Length)
+        {
+            id = (int) (id /Math.pow(10.0,length - Max_Length ));
+        }
+        return  id;
+    }
+
+    /**
+     * Gestione Prenotazione
+     * @return
+     */
+    public boolean prenotaVeicolo(Database db) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cosa vuoi nolleggiare ?" + "\n" + "1- Auto" + "\n" + "2- Scooter" + "\n" + "3- Furgone" + "\n" + "4- Monopattino" + "\n" + "5-Bicicletta");
+        int r = sc.nextInt();
+        switch (r) {
+            case 1:
+                if(db.isDisponibile(new Auto(true, 100, "we345ty", Patente.B))){
+
+                    Map<Integer,Veicolo> veicoli = db.getVeicoli();
+                    Set<Integer> idVeicoliNoleggiati = db.idVeicoliNoleggiati();
+                    Set<Integer> idVeicoli = veicoli.keySet();
+                    idVeicoli.removeAll(idVeicoliNoleggiati);
+                    for(Integer s : idVeicoli){
+                        Veicolo auto = veicoli.get(s);
+                        if(auto  instanceof  Auto){
+                            Noleggio noleggio = new Noleggio(s,15.70);
+                            db.addNoleggio(noleggio);
+                            System.out.println("Hai noleggiato il veicolo "+s);
+                            return true;
+                        }
+
+                    }
+                }else{
+                   System.out.println("Il veicolo scelto non è disponibile");
+                    return false;
+
+                }
+                break;
+            case 2:
+                if(db.isDisponibile(new Scooter(true, 100, "we345ty", Patente.B))){
+
+                    Map<Integer,Veicolo> veicoli = db.getVeicoli();
+                    Set<Integer> idVeicoliNoleggiati = db.idVeicoliNoleggiati();
+                    Set<Integer> idVeicoli = veicoli.keySet();
+                    idVeicoli.removeAll(idVeicoliNoleggiati);
+                    for(Integer s : idVeicoli){
+                        Veicolo scooter = veicoli.get(s);
+                        if(scooter  instanceof  Scooter){
+                            Noleggio noleggio = new Noleggio(s,15.70);
+                            db.addNoleggio(noleggio);
+                            System.out.println("Hai noleggiato il veicolo "+s);
+                            return true;
+                        }
+
+                    }
+                }else{
+                    System.out.println("Il veicolo scelto non è disponibile");
+                    return false;
+
+                }
+                break;
+            case 3:
+                if(db.isDisponibile(new Furgone(true, 100, "we345ty", Patente.B))){
+
+                    Map<Integer,Veicolo> veicoli = db.getVeicoli();
+                    Set<Integer> idVeicoliNoleggiati = db.idVeicoliNoleggiati();
+                    Set<Integer> idVeicoli = veicoli.keySet();
+                    idVeicoli.removeAll(idVeicoliNoleggiati);
+                    for(Integer s : idVeicoli){
+                        Veicolo furgone = veicoli.get(s);
+                        if(furgone  instanceof  Furgone){
+                            Noleggio noleggio = new Noleggio(s,15.70);
+                            db.addNoleggio(noleggio);
+                            System.out.println("Hai noleggiato il veicolo "+s);
+                            return true;
+                        }
+
+                    }
+                }else{
+                    System.out.println("Il veicolo scelto non è disponibile");
+                    return false;
+
+                }
+                break;
+            case 4:
+                if(db.isDisponibile(new Monopattino(true, 100))){
+
+                    Map<Integer,Veicolo> veicoli = db.getVeicoli();
+                    Set<Integer> idVeicoliNoleggiati = db.idVeicoliNoleggiati();
+                    Set<Integer> idVeicoli = veicoli.keySet();
+                    idVeicoli.removeAll(idVeicoliNoleggiati);
+                    for(Integer s : idVeicoli){
+                        Veicolo monop = veicoli.get(s);
+                        if(monop  instanceof  Monopattino){
+                            Noleggio noleggio = new Noleggio(s,15.70);
+                            db.addNoleggio(noleggio);
+                            System.out.println("Hai noleggiato il veicolo "+s);
+                            return true;
+                        }
+
+                    }
+                }else{
+                    System.out.println("Il veicolo scelto non è disponibile");
+                    return false;
+
+                }
+                break;
+            case 5:
+                if(db.isDisponibile(new Bicicletta(true))){
+
+                    Map<Integer,Veicolo> veicoli = db.getVeicoli();
+                    Set<Integer> idVeicoliNoleggiati = db.idVeicoliNoleggiati();
+                    Set<Integer> idVeicoli = veicoli.keySet();
+                    idVeicoli.removeAll(idVeicoliNoleggiati);
+                    for(Integer s : idVeicoli){
+                        Veicolo bici = veicoli.get(s);
+                        if(bici  instanceof  Bicicletta){
+                            Noleggio noleggio = new Noleggio(s,15.70);
+                            db.addNoleggio(noleggio);
+                            System.out.println("Hai noleggiato il veicolo "+s);
+                            return true;
+                        }
+
+                    }
+                }else{
+                    System.out.println("Il veicolo scelto non è disponibile");
+                    return false;
+
+                }
+                break;
+            default:
+                System.out.println("Veicolo non in lista");
+                return false;
+        }return false;
+    }
+
+    public boolean rilasciaVeicolo(Database db,Veicolo veicolo){
+
+        List<Noleggio> noleggio = db.getNoleggi();
+        for(Noleggio  n : noleggio){
+            if(n.getIdVeicoloNoleggiato()==veicolo.getId()) {
+                db.removeNoleggio(n);
+                System.out.println("Hai rilasciato il veicolo " + veicolo.getId());
+                return true;
+            }
+        }
+        System.out.println("Il veicolo non è noleggiato");
+        return false;
+    }
+
+
+
 
 
     @Override
@@ -111,4 +274,5 @@ public class Utente {
     public int hashCode() {
         return Objects.hash(id);
     }
+
 }
